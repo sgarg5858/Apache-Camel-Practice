cf7c7456463e3517f150f86c8a8ff80280370969
Sanjay Garg <33220571+sgarg5858@users.noreply.github.com>
2020-03-05 17:02:44 +0530

    Create OnException2.java

---
+++ OnException2.java
@@ -0,0 +1,52 @@
+package com.practice.DefaultErrorHandler;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+
+public class OnException {
+	public static void main(String[] args) throws Exception {
+		
+		OrderService orderService=new OrderService();
+		SimpleRegistry registry=new SimpleRegistry();
+		registry.put("orderService", orderService);
+		
+		CamelContext camelContext=new DefaultCamelContext(registry);
+		
+		camelContext.addRoutes(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				
+				//context scope
+				errorHandler(defaultErrorHandler()
+						.maximumRedeliveries(2)
+						.redeliveryDelay(1l)
+						.retryAttemptedLogLevel(LoggingLevel.WARN));
+				
+				onException(OrderServiceException.class)
+				.handled(true)
+				.bean(FailureResponseBean.class,"failure");
+
+				//route scope
+				from("direct:start")
+				.bean(OrderService.class, "validate")
+				.bean(OrderService.class,"check")
+				.to("seda:end");
+				
+				
+			}
+		});
+		camelContext.start();
+		ProducerTemplate producerTemplate=camelContext.createProducerTemplate();
+		producerTemplate.sendBody("direct:start","ActiveMQ in Action");
+		
+		ConsumerTemplate consumerTemplate=camelContext.createConsumerTemplate();
+		String message=consumerTemplate.receiveBody("seda:end", String.class);
+		System.out.println(message);
+	}
+}
